fea59fe5fd11a2b225252290af159bb5de11b3dd
Solod Sergey <Solod098@gmail.com>
2022-08-10 14:51:31 +0300

    task2 - ready

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+// делаем переменную => єлемент UL
+const createItemMarkUp = (ingredients) => {
+  const massiveForReturn = [];
+  ingredients.map((ingerdient) => {
+    const itemEl = document.createElement("li");
+    itemEl.classList.add("item");
+    itemEl.textContent = ingerdient;
+    massiveForReturn.push(itemEl);
+  });
+  return massiveForReturn;
+};
+// делаем функцию которая принимает массив с примитивами и возвращает массив разметки
+ingredientsListEl.append(...createItemMarkUp(ingredients));
+//отправляем все в документ
