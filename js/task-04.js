6cdcb6f3d610d21dbbfa1751b4deba9ed99a6d03
Solod Sergey <Solod098@gmail.com>
2022-08-11 13:07:42 +0300

    correction + description

--- js/task-04.js
+++ js/task-04.js
@@ -6,26 +6,35 @@ const counterValue = {
   increment() {
     this.value += 1;
   },
+  reset() {
+    this.value = 0;
+  },
 };
 
+//делаем объект счетчика + методы объекта (+1 и -1)
+
 const valueEl = document.querySelector("#value");
+// делаем переменную => єлемент отображения значений (табло)
 
 const btnDecrementEl = document.querySelector("[data-action = decrement]");
+// делаем переменную => єлемент кнопка +1
 
 const btnIncrementEl = document.querySelector("[data-action = increment]");
+// делаем переменную => єлемент кнопка -1
 const resetBtn = document.querySelector(".reset");
 
 btnDecrementEl.addEventListener("click", () => {
   counterValue.decrement();
   valueEl.textContent = counterValue.value;
 });
-
+// при выполнении клика => value в counterValue с помощью метода объекта будет становиться -1
 btnIncrementEl.addEventListener("click", () => {
   counterValue.increment();
   valueEl.textContent = counterValue.value;
 });
-
+// при выполнении клика => value в counterValue с помощью метода объекта будет становиться +1
 resetBtn.addEventListener("click", () => {
-  counterValue.value = 0;
+  counterValue.reset();
   valueEl.textContent = counterValue.value;
 });
+// при выполнении клика => value в counterValue с помощью метода объекта будет становиться 0
