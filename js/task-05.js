e2feb0ab389ecd9f980f7731c1b01367f1bd1b46
Solod Sergey <Solod098@gmail.com>
2022-08-12 15:03:53 +0300

    task5 - ready

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
+  onInputWhenInput(event) {
+    refs.output.textContent = event.currentTarget.value;
+    // то что вбиваю в инпут (ключ - input), будет в спане (ключ - output)
+    if (refs.output.textContent === "") {
+      // если в output пустая строка
+      refs.output.textContent = "Anonymous";
+      // пустая строка будет = Anonymous
+    }
+  },
+};
+// делаю объект «Объект ссылок»
+// в нем делаю ключи = элементы
+// делаю метод объекта
+
+refs.input.addEventListener("input", refs.onInputWhenInput);
+// делаем input слушателем события и при вводе (действие "input")  + выполняем колбек функцию
