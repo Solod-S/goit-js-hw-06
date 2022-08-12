3e15937f11aa4d30825db27b14845b29a61022ce
Solod Sergey <Solod098@gmail.com>
2022-08-12 17:56:38 +0300

    task9 - ready

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const refs = {
+  body: document.querySelector("body"),
+  display: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+  changeColorFn() {
+    refs.body.style.backgroundColor = refs.getRandomHexColor();
+    refs.display.textContent = refs.body.style.backgroundColor;
+  },
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+};
+
+// refs.body.style.backgroundColor = refs.getRandomHexColor();
+refs.changeColorBtn.addEventListener("click", refs.changeColorFn);
