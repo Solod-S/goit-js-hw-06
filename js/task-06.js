722777ae532d6e5c822f387461101ac102c4704b
Solod Sergey <Solod098@gmail.com>
2022-08-12 16:58:23 +0300

    task6 correction

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,8 @@
 const refs = {
   input: document.querySelector("#validation-input"),
-  onInputWhenInput(event) {
+  onInputWhenBlur(event) {
     if (event.currentTarget.value.length < refs.input.dataset.length) {
+      // console.log("!");
       // refs.input.dataset.length === атрибут data-length="6" в input
       // если длинна введенной строки в инпуте меньше заданной цифры в атрибуте data-length
       refs.input.classList.add("invalid");
@@ -16,13 +17,12 @@ const refs = {
       refs.input.classList.remove("invalid");
       refs.input.classList.remove("valid");
     }
-    console.log(event.currentTarget.value.length);
+    // console.log(event.currentTarget.value.length);
   },
 };
 // делаю объект «Объект ссылок»
 // в нем делаю ключи = элементы
 // делаю метод объекта
 
-refs.input.addEventListener("input", refs.onInputWhenInput);
-// делаем input слушателем события и при вводе (действие "input")  + выполняем колбек функцию из обьекта
-console.log(refs.input.textContent.length);
+refs.input.addEventListener("blur", refs.onInputWhenBlur);
+// делаем input слушателем события и при вводе (действие "blur")  + выполняем колбек функцию из обьекта
